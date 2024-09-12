dc5a7800e653659787a48460ae013b7ba3297f57
hiltuf <hiltuf056@ihmeducation.se>
2024-09-12 14:04:47 +0200

    Updated

---
+++ P5-Operators/js/app.js
@@ -0,0 +1,23 @@
+let kmh = 72;
+let ms = kmh / 3.6;
+
+console.log(kmh);
+console.log(ms)
+
+let minutes = 3;
+let seconds = minutes * 60;
+console.log(minutes);
+console.log(seconds);
+
+let firstNumber = 20;
+let secondNumber = 5;
+let sumAddition = firstNumber + secondNumber;
+let sumDivison = firstNumber / secondNumber;
+console.log(sumAddition);
+console.log(sumDivison);
+
+let sumModule = firstNumber % secondNumber;
+console.log(sumDivison);
+console.log(sumModule); // It will be zero because it will be nothing left.
+
+
