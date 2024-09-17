4b8c8559003fe3ecf4f01b4a8055bfbed9b6ee0f
hiltuf <hiltuf056@ihmeducation.se>
2024-09-17 11:50:06 +0200

    Created P11-For Loops

---
+++ P11ForLoops/js/app.js
@@ -0,0 +1,20 @@
+// First exercise
+for (let first = 0; first < 13; first++) {
+  console.log(Math.floor(Math.random() * 7));
+}
+
+console.log('Second exercise');
+
+// Second exercise
+for (let second = 1; second <= 1024; second*=2) {
+  console.log(second);
+}
+
+console.log('Third exercise');
+// Third exercise
+for (let third = 10; third >= 0; third--) {
+  console.log(third);
+}
+
+console.log('Fourth exercise');
+
